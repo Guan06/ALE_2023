cb02b34d9e0bf356de941541e9f2929a363cf827
Guan06 <rg684@cam.ac.uk>
2025-09-15 18:47:53 +0100

    update filter

---
+++ 99.20250827_ALE/02.call_variant_snippy/rename_bam.sh
@@ -0,0 +1,8 @@
+mv Plate4G2/snps.bam Plate4G2/Plate4G2_snps.bam
+mv Plate4G3/snps.bam Plate4G3/Plate4G3_snps.bam
+mv Plate4H3/snps.bam Plate4H3/Plate4H3_snps.bam
+mv Plate4H5/snps.bam Plate4H5/Plate4H5_snps.bam
+mv Plate4G2/snps.bam.bai Plate4G2/Plate4G2_snps.bam.bai
+mv Plate4G3/snps.bam.bai Plate4G3/Plate4G3_snps.bam.bai
+mv Plate4H3/snps.bam.bai Plate4H3/Plate4H3_snps.bam.bai
+mv Plate4H5/snps.bam.bai Plate4H5/Plate4H5_snps.bam.bai
